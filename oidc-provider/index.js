559ca8f05dac73931f4856b3aa8c9c32e29fad83
Andrea Colli-Vignarelli <s290348@studenti.polito.it>
2022-05-24 19:13:15 +0200

    Added more complex example working

--- oidc-provider/index.js
+++ oidc-provider/index.js
@@ -1,16 +1,78 @@
-const { Provider } = require('oidc-provider');
-const configuration = {
-  // ... see /docs for available configuration
-  clients: [{
-    client_id: 'foo',
-    client_secret: 'bar',
-    redirect_uris: ['http://lvh.me:8080/cb'],
-    // ... other client properties
-  }],
-};
+/* eslint-disable no-console */
 
-const oidc = new Provider('http://localhost:3000', configuration);
+const path = require('path');
+const url = require('url');
 
-oidc.listen(3000, () => {
-  console.log('oidc-provider listening on port 3000, check http://localhost:3000/.well-known/openid-configuration');
+const express = require('express'); // eslint-disable-line import/no-unresolved
+const helmet = require('helmet');
+
+const { Provider } = require('oidc-provider'); // require('oidc-provider');
+
+const Account = require('./support/account');
+const configuration = require('./support/configuration');
+const routes = require('./routes/express');
+
+// Retrieve port from env variable (in the call from the terminal) or set to default values
+const { PORT = 3000, ISSUER = `http://localhost:${PORT}` } = process.env;
+configuration.findAccount = Account.findAccount;
+
+const app = express();
+
+const directives = helmet.contentSecurityPolicy.getDefaultDirectives();
+delete directives['form-action'];
+app.use(helmet({
+  contentSecurityPolicy: {
+    useDefaults: false,
+    directives,
+  },
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+let server;
+(async () => {
+  let adapter;
+  // Start connection to DB if MONGODB_URI is passed as env var
+  if (process.env.MONGODB_URI) {
+    adapter = require('./adapters/mongodb'); // eslint-disable-line global-require
+    await adapter.connect();
+  }
+
+  const prod = process.env.NODE_ENV === 'production';
+
+  const provider = new Provider(ISSUER, { adapter, ...configuration });
+
+  // If we are in production enviroment some changes need attention
+  if (prod) {
+    app.enable('trust proxy');
+    provider.proxy = true;
+
+    app.use((req, res, next) => {
+      if (req.secure) {
+        next();
+      } else if (req.method === 'GET' || req.method === 'HEAD') {
+        res.redirect(url.format({
+          protocol: 'https',
+          host: req.get('host'),
+          pathname: req.originalUrl,
+        }));
+      } else {
+        res.status(400).json({
+          error: 'invalid_request',
+          error_description: 'do yourself a favor and only use https',
+        });
+      }
+    });
+  }
+
+  routes(app, provider);
+  app.use(provider.callback());
+  server = app.listen(PORT, () => {
+    console.log(`application is listening on port ${PORT}, check its /.well-known/openid-configuration`);
+  });
+})().catch((err) => {
+  if (server && server.listening) server.close();
+  console.error(err);
+  process.e
 });
---
+++ oidc-rp/index.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const passport = require('passport');
+const http    = require("http");
+
+const { Issuer,Strategy } = require('openid-client');
+
+const path = require("path");
+
+
+const app = express();
+
+app.use(cookieParser());
+app.use(express.urlencoded({
+  extended: true,
+}));
+
+
+app.use(express.json({ limit: '15mb' }));
+app.use(session({secret: 'secret', 
+                 resave: false, 
+                 saveUninitialized: true,}));
+app.use(helmet());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    console.log('-----------------------------');
+    console.log('serialize user');
+    console.log(user);
+    console.log('-----------------------------');
+    done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+    console.log('-----------------------------');
+    console.log('deserialize user');
+    console.log(user);
+    console.log('-----------------------------');
+    done(null, user);
+});
+
+Issuer.discover('http://localhost:3000') 
+  .then(function (oidcIssuer) {
+    var client = new oidcIssuer.Client({
+      client_id: 'oidcCLIENT',
+      client_secret: 'Some_super_secret',
+      redirect_uris: ["http://localhost:8080/login/callback"],
+      response_types: ['code'], 
+      
+    });
+
+    passport.use(
+      'oidc',
+      new Strategy({ client,passReqToCallback: true}, (req, tokenSet, userinfo, done) => {
+        console.log("tokenSet",tokenSet);
+        console.log("userinfo",userinfo);
+        req.session.tokenSet = tokenSet;
+        req.session.userinfo = userinfo;
+        return done(null, tokenSet.claims());
+      }
+      
+      )
+    );
+  });
+
+
+
+app.get('/login',
+function (req, res, next) {
+    console.log('-----------------------------');
+    console.log('/Start login handler');
+    next();
+},
+passport.authenticate('oidc',{scope:"openid"}));
+
+app.get('/login/callback',(req,res,next) =>{
+
+  passport.authenticate('oidc',{ successRedirect: '/user',
+  failureRedirect: '/' })(req, res, next)
+}
+  
+)
+
+app.get("/",(req,res) =>{
+   res.send(" <a href='/login'>Log In with OAuth 2.0 Provider </a>")
+})
+app.get ("/user",(req,res) =>{
+    res.header("Content-Type",'application/json');
+    res.end(JSON.stringify({tokenset:req.session.passport.user.at_hash,userinfo:req.session.passport.user},null,2));
+
+})
+
+  const httpServer = http.createServer(app)
+  //const server= https.createServer(options,app).listen(3003);
+  httpServer.listen(8080,() =>{
+      console.log(`Http Server Running on port 3004`)
+    })
