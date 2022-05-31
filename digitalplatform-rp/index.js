fb18b137c2715dda89e0fe0104a181b856198580
Andrea Colli-Vignarelli <s290348@studenti.polito.it>
2022-05-31 19:40:01 +0200

    DigitalPlatform RP added

--- digitalplatform-rp/index.js
+++ digitalplatform-rp/index.js
@@ -13,6 +13,7 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 const { Issuer,Strategy } = require('openid-client');
 
 const path = require("path");
+const { userInfo } = require('os');
 
 
 const app = express();
@@ -27,7 +28,12 @@ app.use(express.json({ limit: '15mb' }));
 app.use(session({secret: 'secret', 
                  resave: false, 
                  saveUninitialized: true,}));
-app.use(helmet());
+app.use(helmet({
+  crossOriginEmbedderPolicy: false
+}));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -46,12 +52,12 @@ passport.deserializeUser(function(user, done) {
     done(null, user);
 });
 
-Issuer.discover('https://10.0.0.80:3000') 
+Issuer.discover('https://localhost:3000') 
   .then(function (oidcIssuer) {
     var client = new oidcIssuer.Client({
       client_id: 'digitalPlatformRP',
       client_secret: 'Some_super_secret',
-      redirect_uris: ["https://10.0.0.202:8080/login/callback"],
+      redirect_uris: ["https://localhost:8080/login/callback"],
       response_types: ['code'], 
       
     });
@@ -74,7 +80,8 @@ Issuer.discover('https://10.0.0.80:3000')
     helmet.contentSecurityPolicy({
       useDefaults: true,
       directives: {
-        "img-src": ["'self'", "https: data:"]
+        "img-src": ["'self'", "https: data:"],
+        "script-src": ["'self'", "https: data:"]
       }
     })
   )
@@ -87,9 +94,17 @@ function (req, res, next) {
 },
 passport.authenticate('oidc',{scope:"openid"}));
 
+function loggedIn(req, res, next) {
+  if (req.user) {
+      next();
+  } else {
+      res.redirect('/');
+  }
+}
+
 app.get('/login/callback',(req,res,next) =>{
 
-  passport.authenticate('oidc',{ successRedirect: '/user',
+  passport.authenticate('oidc',{ successRedirect: '/services',
   failureRedirect: '/' })(req, res, next)
 }
   
@@ -99,9 +114,33 @@ app.get("/",(req,res) =>{
   res.sendFile(path.join(__dirname+'/views/index.html'));
    //res.send(" <a href='/login'>Log In with OAuth 2.0 Provider </a>")
 })
-app.get ("/user",(req,res) =>{
-    res.header("Content-Type",'application/json');
-    res.end(JSON.stringify({tokenset:req.session.passport.user.at_hash,userinfo:req.session.passport.user},null,2));
+
+app.get ("/services", loggedIn, (req,res) =>{
+    /*res.header("Content-Type",'application/json');
+    res.end(JSON.stringify({tokenset:req.session.passport.user.at_hash,userinfo:req.session.passport.user},null,2));*/
+    let tokenset = req.session.passport.user.at_hash;
+    let userinfo = req.session.passport.user;
+    res.render(path.join(__dirname+'/views/services'), {
+      user: userinfo
+    })
+})
+
+app.get ("/services/inps", loggedIn, (req,res) =>{
+  let tokenset = req.session.passport.user.at_hash;
+  let userinfo = req.session.passport.user;
+  res.render(path.join(__dirname+'/views/inps'), {
+    user: userinfo
+  })
+})
+
+app.get ("/user", loggedIn, (req,res) =>{
+  /*res.header("Content-Type",'application/json');
+  res.end(JSON.stringify({tokenset:req.session.passport.user.at_hash,userinfo:req.session.passport.user},null,2));*/
+  let tokenset = req.session.passport.user.at_hash;
+  let userinfo = req.session.passport.user;
+  res.render(path.join(__dirname+'/views/services'), {
+    user: userinfo
+  })
 
 })
 
@@ -111,7 +150,7 @@ const options = {
 };
 
   //const httpServer = http.createServer(app)
-  const server= https.createServer(options,app).listen(8080, "10.0.0.202");
+  const server= https.createServer(options,app).listen(8080);
   /*httpServer.listen(8081,() =>{
       console.log(`Http Server Running on port 8081`)
       console.log('http://localhost:8081')
