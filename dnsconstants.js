1c4c8ea3fd03a156868f597f121e37380c8c486a
teodoro98 <teodosiobla@gmail.com>
2022-06-05 17:07:05 +0200

    scalable constant added

--- dnsconstants.js
+++ dnsconstants.js
@@ -1,28 +1,39 @@
+const digitalplatform_dns = "digitalplatform.ddns.net"
+const digitalplatform_port = "443"
+
+const inps_dns = "fakeinps.ddns.net"
+const inps_port = "443"
+
+const provider_dns = "cieprovider.ddns.net"
+const provider_port = "443"
+
+
+
 const digitalplatform = {
-    dns : 'digitalplatform.ddns.net',
-    url : 'https://digitalplatform.ddns.net',
-    port : 443,
-    site : 'https://digitalplatform.ddns.net:443',
+    dns : digitalplatform_dns,
+    url : "https://"+digitalplatform_dns,
+    port : digitalplatform_port,
+    site : "https://"+digitalplatform_dns+":"+digitalplatform_port,
     client_id: 'digitalPlatformClient',
     client_secret: 'Some_super_secret',
-    redirect_uris: ["https://digitalplatform.ddns.net:443/login/callback"],
+    redirect_uris: ["https://"+digitalplatform_dns+":"+digitalplatform_port+"/login/callback"],
 }
 
 const inps = {
-    dns : 'fakeinps.ddns.net',
-    url : 'https://fakeinps.ddns.net',
-    port : 443,
-    site : 'https://fakeinps.ddns.net:443',
+    dns : inps_dns,
+    url : "https://"+inps_dns,
+    port : inps_port,
+    site : "https://"+inps_dns+":"+inps_port,
     client_id: 'inpsClient',
     client_secret: 'Some_super_secret',
-    redirect_uris: ["https://fakeinps.ddns.net:443/login/callback"],
+    redirect_uris: ["https://"+inps_dns+":"+inps_port+"/login/callback"],
 }
 
 const provider = {
-    dns : 'cieprovider.ddns.net',
-    url : 'https://cieprovider.ddns.net',
-    port : 443,
-    site : 'https://cieprovider.ddns.net:443',
+    dns : provider_dns,
+    url : "https://"+provider_dns,
+    port : provider_port,
+    site : "https://"+provider_dns+":"+provider_port,
 }
 
 module.exports= {
